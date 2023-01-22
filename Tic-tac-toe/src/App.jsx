dd9237c092a81e84f63bfa995cc64b57c2183c43
David Gaspar <davsangas@hotmail.com>
2023-01-22 13:24:51 -0600

    defining winner

--- Tic-tac-toe/src/App.jsx
+++ Tic-tac-toe/src/App.jsx
@@ -20,19 +20,54 @@ const Square = ({ children, isSelected, updateBoard, index }) => {
   )
 }
 
+const WINNER_COMBOS = [
+  [0, 1, 2],//
+  [0, 4, 8],//
+  [0, 3, 6],//
+  [1, 4, 7],//
+  [2, 5, 8],//
+  [2, 4, 6],//
+  [3, 4, 5],//
+  [6, 7, 8],//
+]
+
 function App() {
   const [board, setBoard] = useState(Array(9).fill(null))//Tablero
 
   const [turn, setTurn] = useState(TURNS.X)
 
+  const [winner, setWinner] = useState(null)//Null sin ganador, false empate
+
+  const CheckWinner = (boardToCheck) => {
+    //Revisamos todas las combinaciones ganadoras
+    for (const combo of WINNER_COMBOS) {
+      const [a, b, c] = combo
+      if(boardToCheck[a] &&
+        boardToCheck[a] === boardToCheck[b] &&
+        boardToCheck[a] === boardToCheck[c])
+        {
+        return boardToCheck[a]
+      }
+    }
+    //Si no hay ganador.
+    return null
+  }
+
   const updateBoard = (index) => {
-    if(board[index]) return
+    //No actualizar si hay ganador o si ya hay una ficha
+    if(board[index] || winner) return
+    //Actualizar el tablero
     const newBoard = [...board]
     newBoard[index] = turn
     setBoard(newBoard)
-
+    //Cambiar el turno
     const newTurn = turn === TURNS.X ? TURNS.O : TURNS.X
     setTurn(newTurn)
+    //Checar ganador
+    const NewWinner = CheckWinner(newBoard)
+    if(NewWinner) {
+      setWinner(NewWinner)
+    }
   }
   console.log('updateBoard')
 
